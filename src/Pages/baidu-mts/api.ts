d7682d092b548d5d43ed22f490dbce8116f629e7
Yucong Zhou <zhouyucong0421@foxmail.com>
2021-05-28 22:33:06 +0800

    feat: mts page api interface and structure

---
+++ src/Pages/baidu-mts/api.ts
@@ -0,0 +1,14 @@
+import qs, { ParsedUrlQueryInput } from 'querystring'
+import HttpRequest from '@/Request';
+
+const request = HttpRequest.GetInstance();
+
+export const GetTreeNames = async () => {
+    const ret = request.get('/api/mts/trees');
+    return ret;
+}
+
+export const GetTreeIndex = async (params) => {
+    const ret = request.get(`/api/mts/tree?${qs.stringify(params)}`);
+    return ret;
+}
--- src/Pages/baidu-mts/index.tsx
+++ src/Pages/baidu-mts/index.tsx
@@ -1,10 +1,86 @@
-import React, { FunctionComponent } from 'react';
-import {} from 'antd';
+import React, { FunctionComponent, useCallback, useEffect, useMemo, useState } from 'react';
+import { Card, Form, Select, Button, message } from 'antd';
+import { RedoOutlined } from '@ant-design/icons';
 
 import render from '@/utils/render';
+import { GetTreeIndex, GetTreeNames } from './api';
+
+import './index.less';
 
 const BaiduMtsPage = () => {
-    return null;
+    const [treeNames, setTreeNames] = useState([]);
+    const [selectedName, setSelectedName] = useState(null);
+    const [refreshLoading, setRefreshLoading] = useState(false);
+    const [treeIndex, setTreeIndex] = useState();
+    const [indexLoading, setIndexLoading] = useState(false);
+
+    const getTreeNamesCallback = useCallback(() => {
+        setRefreshLoading(true);
+        GetTreeNames().then(res => {
+            setTreeNames([...res.data])
+            if (!selectedName || res.data.indexOf(selectedName) === -1) {
+                setSelectedName(res.data[0])
+            }
+        }).finally(() => {
+            setRefreshLoading(false);
+        });
+    }, [selectedName]);
+
+    const getTreeIndex = useCallback(() => {
+        const params = {
+            name: selectedName,
+        }
+        setIndexLoading(true)
+        GetTreeIndex(params).then(res => {
+            const nodes = res.data;
+            message.info(`采样了${nodes.length}个节点`);
+        }).finally(() => {
+            setIndexLoading(false);
+        })
+    }, [selectedName]);
+
+    useEffect(() => {
+        getTreeNamesCallback();
+    }, []);
+
+    return (<div className="baidu-mts-page">
+        <div className="left-part">
+            <Card title="一些配置">
+                <Form>
+                    <Form.Item>
+                        <Button
+                            icon={<RedoOutlined />}
+                            onClick={getTreeNamesCallback}
+                            disabled={refreshLoading}
+                        />
+                        <Select
+                            className="tree-selection"
+                            value={selectedName}
+                            onChange={setSelectedName}
+                        >
+                            {treeNames.map(name => (<Select.Option
+                                value={name}
+                                key={name}
+                            >
+                                {name}
+                            </Select.Option>))}
+                        </Select>
+                        <Button
+                            onClick={getTreeIndex}
+                            type="primary"
+                        >
+                            确认
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
+        <div className="right-part">
+            <Card title="可视化">
+
+            </Card>
+        </div>
+    </div>);
 };
 
 render(<BaiduMtsPage />);
